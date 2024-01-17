377c29b9ad0810e6a5d35b853774a78783ee4d01
Alejandro-Arroyave <achoarro@hotmail.com>
2024-01-16 20:21:37 -0500

    adding communication between container and marketing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,26 +2,22 @@ import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom";
+import { Router, Route, Switch } from "react-router-dom/cjs/react-router-dom";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "ma",
 });
 
-const App = () => {
+const App = ({ history }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route exact path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  const onParentNavigate = ({ pathname: nextPathname }) => {
+    if (nextPathname) {
+      history.push(nextPathname);
+    }
+  };
+
+  return {
+    onParentNavigate,
+  };
 };
 
 // Two scenarios: Development or isolation call immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 // Through container and we should export mount
