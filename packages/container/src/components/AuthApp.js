73eed4c2895b900f5352df7df2ca6fc5da1609f4
Alejandro-Arroyave <achoarro@hotmail.com>
2024-03-06 21:39:13 -0500

    Section 12: Deploying auth project

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,12 +2,13 @@ import React, { useRef, useEffect } from "react";
 import { mount } from "auth/AuthApp";
 import { useHistory } from "react-router-dom";
 
-const AuthApp = () => {
+const AuthApp = ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
+      onSignIn,
       initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
